c0f86f7e7494e82874dcdc98ec2a6ea847d818e5
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-03-09 15:58:12 -0800

    Add user authentication and registration

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  before_action :authorize
+
   def show
     @products = Product.all
     @categories = Category.all
